611d323e3094ee32d40f442b36b7e9405e1645a7
malodev1234 <obrayanleader@gmail.com>
2023-05-02 15:32:47 +0200

    Feat componnet

--- scriptcv.js
+++ scriptcv.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Trainner", "Developer Web", "Developer Mobile", "Designer"],
+        strings: ["Software Engineer", "Fullstack Web Developer", "Fullstack Mobile Developer", "UX/UI Designer"," And Web3 developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Trainner", "Developer Web", "Developer Mobile", "Designer"],
+        strings: ["Software Engineer", "Fullstack Web Developer", "Fullstack Mobile Developer", "UX/UI Designer", "And Web3 developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
